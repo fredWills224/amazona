57e7e78111534408c22689c50b4acfa80d0a531e
fredWills224 <fredwills4229@gmail.com>
2020-11-29 03:25:40 -0500

    node server

---
+++ backend/data.js
@@ -0,0 +1,45 @@
+export default {
+    products:[
+        {
+            _id: '1',
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            category: 'Shirts',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            image: '/images/d1.jpg',
+            category: 'Shirts',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 5
+        
+        },
+        {
+            _id: '3',
+            name: 'Best pants',
+            image: '/images/d1.jpg',
+            category: 'Pants',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8
+        },
+        {
+            _id: '4',
+            name: 'Best pants',
+            image: '/images/d1.jpg',
+            category: 'Pants',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8
+        }
+    ]
+}
