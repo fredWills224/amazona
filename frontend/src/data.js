d6865e029e279a498c9569ac86d62b18611d1137
fredWills224 <fredwills4229@gmail.com>
2020-11-28 23:02:40 -0500

    render products {jsx}

---
+++ frontend/src/data.js
@@ -0,0 +1,32 @@
+export default {
+    products:[
+        {
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            category: 'Shirts',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Fit Shirt',
+            image: '/images/d1.jpg',
+            category: 'Shirts',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 5
+        
+        },
+        {
+            name: 'Best pants',
+            image: '/images/d1.jpg',
+            category: 'Pants',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8
+        }
+    ]
+}
