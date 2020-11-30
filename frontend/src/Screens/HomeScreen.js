3a9b7cf196196a2b4fe47b932350c29f64da385b
fredWills224 <fredwills4229@gmail.com>
2020-11-30 00:37:32 -0500

    manage state with redux

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -2,17 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import data from '../data';
 import axios from 'axios';
+import { useSelector, useDispatch  } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 function HomeScreen (props){
 
-  const [products, setProducts] = useState([]);
+  const productList = useSelector(state => state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchData = async ()=>{
-      const {data} = await axios.get('/api/products');
-      setProducts(data);
-    }
-    fetchData();
+    
+    dispatch(listProducts());
+
     return () => {
       
     }
@@ -20,9 +22,11 @@ function HomeScreen (props){
 
   return (
     
+    loading ? <div>Loading....</div> :
+    error ? <div>{error}</div> :
     <ul className='products'>
       {
-        data.products.map(product =>
+        products.map(product =>
           <li key={product._id}>
             
             <div className='product'>  
